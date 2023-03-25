53f4fec351ef720c3e50a9ae650cd8ba54e12438
Vishal Sharma <fvishalsharm123@gmail.com>
2023-03-25 12:04:30 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import logo from "../assets/Logo.svg";
+import {Link} from "react-router-dom";
+import { toast } from 'react-hot-toast';
+
+const Navbar = (props) => {
+
+  let isLoggedIn = props.isLoggedIn;
+  let setIsLoggedIn = props.setIsLoggedIn;
+  
+
+
+  return (
+    <div className='flex justify-evenly'>
+      <Link to="/">
+        <img src={logo} alt="Logo" width={160} height={32} loading="lazy"></img>
+      </Link>
+
+      <nav>
+        <ul className='flex gap-3'>
+          <li>
+           <Link to="/">Home</Link>
+          </li>
+          <li>
+           <Link to="/">About</Link>
+          </li>
+          <li>
+           <Link to="/">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+     {/* {/* Login - Sign up  - Logout - Dashboard */}
+     <div className='flex ml-5 mr-3 gap-5'>
+      { !isLoggedIn &&
+        <Link to="/login">
+          <button>
+            Login
+          </button>
+        </Link>
+      }
+     </div>
+
+     <div>
+      { !isLoggedIn &&
+        <Link to="/signup">
+          <button>
+            Sign Up
+          </button>
+        </Link>
+      }
+     </div>
+
+     <div>
+      {  isLoggedIn && 
+          <Link to="">
+          <button onClick={() => {
+            setIsLoggedIn(false);
+            toast.success("Logged Out");
+          }}>
+            Log Out
+          </button>
+        </Link>
+      }
+     </div>
+
+     <div>
+      {  isLoggedIn &&
+        <Link to="/dashboard">
+          <button>
+            Dashboard
+          </button>
+        </Link>
+      }
+     </div>
+
+
+    </div> 
+  )
+}
+
+export default Navbar;
---
+++ src/components/SignupForm.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { toast } from 'react-hot-toast';
+
+const SignupForm = ({ setIsLoggedIn }) => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  function changeHandler(event) {
+    setFormData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      toast.error('Password Do not match');
+      return;
+    }
+    setIsLoggedIn(true);
+    toast.success('Account created successfully');
+  }
+
+  return (
+    <div>
+      <h2>Signup Form</h2>
+      {/* {Student Instrutor tab} */}
+      <div>
+        <button>Student</button>
+        <button>Instrutor</button>
+      </div>
+
+      <form onSubmit={submitHandler}>
+        <div>
+          <label>
+            <p>First name <sup>*</sup></p>
+            <input
+              required
+              type="text"
+              name="firstName"
+              onChange={changeHandler}
+              placeholder="Enter first name"
+              value={formData.firstName}
+            />
+          </label>
+
+          <label>
+            <p>Last name <sup>*</sup></p>
+            <input
+              required
+              type="text"
+              name="lastName"
+              onChange={changeHandler}
+              placeholder="Enter last name"
+              value={formData.lastName}
+            />
+          </label>
+        </div>
+
+        <label>
+          <p>Email Address <sup>*</sup></p>
+          <input
+            required
+            type="email"
+            name="email"
+            onChange={changeHandler}
+            placeholder="Enter email id"
+            value={formData.email}
+          />
+        </label>
+
+        <div>
+          <label>
+            <p>Create Password<sub>*</sub></p>
+            <input
+              required
+              type={showPassword ? 'text' : 'password'}
+              value={formData.password}
+              onChange={changeHandler}
+              placeholder="Enter Password"
+              name="password"
+            />
+            <span onClick={() => setShowPassword((prev) => !prev)}>
+              {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </span>
+          </label>
+
+          <label>
+            <p>Confirm Password<sub>*</sub></p>
+            <input
+              required
+              type={showPassword ? 'text' : 'password'}
+              value={formData.confirmPassword}
+              onChange={changeHandler}
+              placeholder="Confrim Password"
+              name="confirmPassword"
+            />
+            <span onClick={() => setShowPassword((prev) => !prev)}>
+              {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </span>
+          </label>
+        </div>
+
+        <button>Create Account</button>
+      </form>
+    </div>
+
+  )
+}
+
+export default SignupForm;
